c08020927eb4133852e1875b568caae979bfcd29
henry-sg <jh.guardado@icloud.com>
2024-01-25 01:03:08 -0600

    Fix

--- cypress/pages/TemplatesPage.js
+++ cypress/pages/TemplatesPage.js
@@ -4,16 +4,6 @@ class TemplatesPage extends BasePage {
   constructor() {
     super("/account/importers");
   }
-
-  elements = {
-    //inputs
-    emailInput: () => cy.get("input#email"),
-    passwordInput: () => cy.get("input#password"),
-
-    //buttons
-    signInButton: () => cy.get("div[data-cy='SignIn']"),
-    signInGithubButton: () => cy.contains("div", "Sign in with Github"),
-  };
 }
 
 module.exports = new TemplatesPage();
